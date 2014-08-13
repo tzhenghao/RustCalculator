ba59df04070cfd7bae7de11ae579ed2c46d4b86a
Zheng Hao Tan <tanzhao@umich.edu>
2014-08-13 22:22:41 +0800

    Added new main.rs file

---
+++ main.rs
@@ -0,0 +1,8 @@
+// Name: Zheng Hao Tan 
+// Email: tanzhao@umich.edu 
+// Date: Aug 13, 2014
+
+// This is the main Rust file that contains the implementation of a basic 
+// calculator program.
+
+
